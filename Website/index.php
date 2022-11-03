0261a2870489e92a65415c2c0d712ccca8c3c639
Keng Xiong <74529239+xiokd@users.noreply.github.com>
2022-11-03 08:54:47 -0700

    Added Font Awesome files

--- Website/index.php
+++ Website/index.php
@@ -1,1 +1,17 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="assets/css/all.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <title>HOME | John Oreo</title>
+    </head>
+    <body>
+    
+        <?php
+
+        ?>
+        
+    </body>
+</html>
