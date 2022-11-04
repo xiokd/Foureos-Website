e0e9fc8c29b05ac5db7ca6927c33482198929acd
Keng Xiong <74529239+xiokd@users.noreply.github.com>
2022-11-04 16:44:56 -0700

    Revised navigation bar implementation

--- Website/contact.php
+++ Website/contact.php
@@ -1,1 +1,17 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="assets/css/all.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <title>Contact | John Oreo</title>
+    </head>
+    <body>
+        <?php
+            include_once("navbar.php");
+        ?>
+        
+    </body>
+</html>
--- Website/index.php
+++ Website/index.php
@@ -2,31 +2,15 @@
 <html>
     <head>
         <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="assets/css/all.css">
         <link rel="stylesheet" href="assets/css/style.css">
-        <title>HOME | John Oreo</title>
+        <title>Home | John Oreo</title>
     </head>
     <body>
-
-        <header class="header">
-            <div class="logo">
-                <a href="index.php">
-                    <i class="fa-solid fa-cookie-bite"></i>
-                </a>
-            </div>
-
-            <nav class="navbar">
-                <a href="aboutme.php">About</a>
-                <a href="currentprojects.php">Projects</a>
-                <a href="resume.php">Resume</a>
-                <a href="contact.php">Contact</a>
-            </nav>
-
-        </header>
-
         <?php
-
+            include_once("navbar.php");
         ?>
         
     </body>
