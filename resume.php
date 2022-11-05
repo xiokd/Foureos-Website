a561d8dfdcb022d0ab4ea0c310360e4563515a85
Keng Xiong <74529239+xiokd@users.noreply.github.com>
2022-11-05 09:39:56 -0700

    Removed Website directory and moved files

---
+++ resume.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="assets/css/all.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <title>Resume | John Oreo</title>
+    </head>
+    <body>
+        <?php
+            include_once("navbar.html");
+        ?>
+        
+    </body>
+</html>
