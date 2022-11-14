ecc1b1f00ff42c595694f065f25e4b811b95a89e
Keng Xiong <74529239+xiokd@users.noreply.github.com>
2022-11-13 22:49:43 -0800

    Moved contact page javascript implementation to a separate file (#16)

--- contact.php
+++ contact.php
@@ -26,38 +26,11 @@
                 </form>
             </div>
         </div>
-        
+
         <?php
           include_once("footer.html");
         ?>
 
-        <script>
-            $(document).ready(function() {
-                $("#submit").click(function() {
-                    var name = $("#name").val();
-                    var email = $("#email").val();
-                    var subject = $("#subject").val();
-                    var message = $("#message").val();
-
-                    var datastring = 'name1=' + name + '&email1=' + email + '&subject1=' + subject + '&message1=' + message;
-
-                    if(name == '' || email == '' || subject == '' || message == '') {
-                        $("#display-message").html('<div class="form-error"> Please fill in all fields </div>');
-                    } else {
-                            $.ajax({
-                                type: "POST",
-                                url: "processor.php",
-                                data: datastring,
-                                cache: false,
-                                success: function(result) {
-                                    $("#display-message").html(result);
-                                }
-                            });
-                    }
-
-                    return false;
-                });
-            });
-        </script>   
+        <script type="text/javascript" src="assets/js/contact-script.js"></script>
     </body>
 </html>
