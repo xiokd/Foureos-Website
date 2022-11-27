2dccd8386081be204c4e54ce928cf24e55e055b5
Brennen Kane-Christensen <bkanechristensen@eagles.ewu.edu>
2022-11-27 14:34:04 -0800

    Altered error colors

--- register_form.php
+++ register_form.php
@@ -12,7 +12,7 @@
 
         $select = " SELECT * FROM user_form WHERE email = '$email' && password = '$pass' ";
 
-        $result = mysqli_query($conn, $select);
+        $result = mysqli_query($aConn, $select);
 
         if(mysqli_num_rows($result) > 0) {
             $error[] = 'user already exists.';
@@ -24,7 +24,8 @@
             }
             else {
                 $insert = "INSERT INTO user_form(name, email, password, user_type) VALUES('$name, $email, $pass, $user_type')";
-                mysqli_query($conn, $insert);
+                mysqli_query($aConn, $insert);
+                header('location:login_form.php');
             }
         }
 
