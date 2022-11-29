1d4b205e1f232ec1891f78d13352887dc8784137
TheHateofTheFox <ngainer@ewu.edu>
2022-11-29 15:44:16 -0800

    fixed up current projects

--- currentprojects.php
+++ currentprojects.php
@@ -1,25 +1,50 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="assets/css/all.css">
-        <link rel="stylesheet" href="assets/css/style.css">
-        <title>Projects | John Oreo</title>
-    </head>
-    <body>
-        <?php
-            include_once("navbar.html");
-        ?>
-        <img src = "assets/images/projectPics/tunafish.jpg" width = "300" height="200" title="The Infamous Tuna Fish Oreo" />
+        <head>
 
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="assets/css/all.css">
+            <link rel="stylesheet" href="assets/css/style.css">
+            <title>Projects | John Oreo</title>
+        </head>
         <style>
-            p {
-                background-image: url('assets/images/projectPics/tunafish.jpg');
+            body{
+                padding-top: 150px;
+                background-image: url("assets/images/projectBackground.jpeg");
+                background-color: #cccccc;
+                background-repeat: repeat;
+                background-size: auto;
+                min-height: 1080px;
+
+                min-width: 1080px;
             }
         </style>
+    <body>
+        <?php
+        include_once("navbar.html");
+        ?>
 
+
+        <div class = "project1">
+            <img src = "assets/images/projectPics/tunafish.jpg" width = "400" height="300" title="The Infamous Tuna Fish Oreo" style="float: left" />
+            <p style="color:rebeccapurple;font-size:20px">the tuna fish Oreo is a personally favorite of mine</p>
+        </div>
+
+        <div class = "project2">
+            <img src = "assets/images/projectPics/AvocadoToast.jpg" width = "400" height="300" title="The Tasty Avocado Treat" style="float: left" />
+            <p style="color:rebeccapurple;font-size:20px">In the creation of this Oreo I was thinking </p>
+        </div>
+        <div class = "project3">
+            <img src = "assets/images/projectPics/cheeseburger.jpg" width = "400" height="300" title="The cheesy surprise" style="float: left" />
+            <p style="color:rebeccapurple;font-size:20px">Who doesn't love a cheeseburgers? and who doesn't love my gloriest oreo cookie? so I thought
+             to my self why not add them together, and boom created this fabulous creation</p>
+        </div>
+        <div class = "project4">
+            <img src = "assets/images/projectPics/Dorito.png" width = "400" height="300" title="The Dorito finger licker" style="float: left" />
+            <p style="color:brown;font-size:20px">I can guarantee that after one of these bad boy's you want stop until the pack is gone</p>
+        </div>
 
     </body>
 </html>
