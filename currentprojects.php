fbcf14d5f237639557448b5bebb40f885ace992a
TheHateofTheFox <ngainer@ewu.edu>
2022-12-01 14:44:12 -0800

    got the project page finished

--- currentprojects.php
+++ currentprojects.php
@@ -1,50 +1,70 @@
 <!DOCTYPE html>
 <html>
-        <head>
+<head>
 
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="stylesheet" href="assets/css/all.css">
-            <link rel="stylesheet" href="assets/css/style.css">
-            <title>Projects | John Oreo</title>
-        </head>
-        <style>
-            body{
-                padding-top: 150px;
-                background-image: url("assets/images/projectBackground.jpeg");
-                background-color: #cccccc;
-                background-repeat: repeat;
-                background-size: auto;
-                min-height: 1080px;
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/all.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Projects | John Oreo</title>
+</head>
+<style>
+    body{
+        padding-top: 150px;
+        background-image: url("assets/images/projectBackground.jpeg");
+        background-color: darkgray;
+        background-repeat: repeat;
+        background-size: auto;
 
-                min-width: 1080px;
-            }
-        </style>
-    <body>
-        <?php
-        include_once("navbar.html");
-        ?>
+    }
+</style>
+<body>
+<?php
+include_once("navbar.html");
+?>
+<section class="currentprojects" id="currentprojects">
 
+    <div class = "project1">
+        <img src = "assets/images/projectPics/tunafish_out.png" width = "400" height="300" title="The Infamous Tuna Fish Oreo" style="float: left" />
+        <p style="color:rebeccapurple;font-size:20px"></p>
+    </div>
+    <div class="rightText">
 
-        <div class = "project1">
-            <img src = "assets/images/projectPics/tunafish_out.png" width = "400" height="300" title="The Infamous Tuna Fish Oreo" style="float: left" />
-            <p style="color:rebeccapurple;font-size:20px">the tuna fish Oreo is a personally favorite of mine</p>
-        </div>
+        <p>the tuna fish Oreo is a personally favorite of mine, it provides the perfect dessert lunch flavor</p>
+    </div>
 
-        <div class = "project2">
-            <img src = "assets/images/projectPics/AvocadoToast_out.png" width = "400" height="300" title="The Tasty Avocado Treat" style="float: left" />
-            <p style="color:rebeccapurple;font-size:20px">In the creation of this Oreo I was thinking </p>
-        </div>
-        <div class = "project3">
-            <img src = "assets/images/projectPics/cheez_out.jpg" width = "400" height="300" title="The cheesy surprise" style="float: left" />
-            <p style="color:rebeccapurple;font-size:20px">Who doesn't love a cheeseburgers? and who doesn't love my gloriest oreo cookie? so I thought
-             to my self why not add them together, and boom created this fabulous creation</p>
-        </div>
-        <div class = "project4">
-            <img src = "assets/images/projectPics/Dorito_out.png" width = "400" height="300" title="The Dorito finger licker" style="float: left" />
-            <p style="color:brown;font-size:20px">I can guarantee that after one of these bad boy's you want stop until the pack is gone</p>
-        </div>
+    <div class = "project2">
+        <img src = "assets/images/projectPics/AvocadoToast_out.png" width = "400" height="300" title="The Tasty Avocado Treat" style="float: left" />
+        <p style="color:rebeccapurple;font-size:20px"><br></p>
+    </div>
 
-    </body>
+    <div class="rightText">
+
+        <p>In the creation of this Oreo I was thinking of a more sophisticated flavor that just hit right</p>
+    </div>
+
+    <div class = "project3">
+        <img src = "assets/images/projectPics/cheez_out.jpg" width = "400" height="300" title="The cheesy surprise" style="float: left" />
+        <p style="color:rebeccapurple;font-size:20px"></p>
+    </div>
+
+    <div class="rightText">
+
+        <p>Who doesn't love a cheeseburgers? and who doesn't love my gloriest oreo cookie? so I thought
+            to my self why not add them together, and boom created this fabulous creation</p>
+    </div>
+
+    <div class = "project4">
+        <img src = "assets/images/projectPics/Dorito_out.png" width = "400" height="300" title="The Dorito finger licker" style="float: left" />
+        <p style="color:brown;font-size:20px"></p>
+    </div>
+
+    <div class="rightText">
+
+        <p>I can guarantee that after one of these bad boy's you want stop until the pack is gone</p>
+    </div>
+
+</section>
+</body>
 </html>
